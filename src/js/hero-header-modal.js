a340597e8b705043b0e57397586f37da3f741c4e
SzymonOgniewski <szymonogniewski00@gmail.com>
2022-09-05 12:49:10 +0200

    added modal and form

--- src/js/hero-header-modal.js
+++ src/js/hero-header-modal.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[header-modal-open]"),
-    closeModalBtn: document.querySelector("[header-modal-close]"),
-    modal: document.querySelector("[header-modal]"),
+    openModalBtn: document.querySelector('[header-modal-open]'),
+    closeModalBtn: document.querySelector('[header-modal-close]'),
+    modal: document.querySelector('[header-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
